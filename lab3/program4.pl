15944bbe6ee84b6c6a7a2e36cbceea7b95c296a0
= <subinayp108@gmail.com>
2024-08-26 21:21:56 +0530

    lab3 updated

--- lab3/program4.pl
+++ lab3/program4.pl
@@ -29,17 +29,23 @@ couple(X, Y):-
 % xor function
 xor(X, Y):- (X, \+ Y); (\+ X, Y).
 
+% or function
+or(true, _).
+or(_, true).
+
 % X is father of Y
 father(X, Y):- parent(X, Y), male(X).
 
 % X is mother of Y
 mother(X, Y):- parent(X, Y), female(X).
 
-% X and Y have common parent
-common_parent(X, Y):- parent(Z, X), parent(Z, Y).
+% X and Y are siblings
+siblings(X, Y):- 
+    setof(Y, P^ (parent(P, X), parent(P, Y), X \== Y), Ys),
+    member(Y, Ys).
 
 % X is brother of Y
-brother(X, Y):- common_parent(X, Y), male(X), X \== Y, X @< Y.
+brother(X, Y):- siblings(X, Y), male(X).
                 
 
 % X is uncle of Y
@@ -51,4 +57,4 @@ uncle(X, Y):- parent(Z, Y), brother(X, Z).
 grandmother(X, Y):- mother(Z, Y), mother(X, Z).
 
 % X is cousin of Y
-cousin(X, Y):- uncle(Z, Y), parent(X, Z);
+cousin(X, Y):- parent(A, X), parent(B, Y), siblings(A, B).
