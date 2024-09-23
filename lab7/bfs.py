b079806600c2e2d0b19b95cac4e2cacd24381549
subinay108 <subinayp108@gmail.com>
2024-09-23 10:18:31 +0530

    lab7 added

---
+++ lab7/bfs.py
@@ -0,0 +1,33 @@
+# WAPP for Breadth First Search
+
+def bfs(graph, start):
+    queue = [start]
+    visited = set()
+    
+    while queue:
+        # print('queue:', queue)
+        node = queue.pop(0)
+        if node not in visited:
+            visited.add(node)
+            print(node, end='->')
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+    
+    
+
+if __name__ == '__main__':
+    graph = { 
+        1: [2, 3, 4], 
+        2: [1, 4, 5],
+        3: [1, 6, 7, 8],
+        4: [1, 2],
+        5: [2],
+        6: [3, 7],
+        7: [3, 6, 8],
+        8: [3, 7]
+    }
+    bfs(graph, 2)
+
+
